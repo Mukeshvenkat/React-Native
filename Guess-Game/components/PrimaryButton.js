5550b5091a0916da6da8d12b728f4efaf807e0f0
MukeshVenkat <mukiv0802@gmail.com>
2023-10-03 13:06:25 +0530

    Guess-Game : Components and Screens added.

---
+++ Guess-Game/components/PrimaryButton.js
@@ -0,0 +1,41 @@
+import { Pressable, StyleSheet, Text, View } from "react-native";
+
+function PrimaryButton({ children }) {
+  return (
+    <View style={styles.buttonOuterConatiner}>
+      <Pressable
+        style={({ pressed }) =>
+          pressed
+            ? [styles.buttonInnerConatiner, styles.pressed]
+            : styles.buttonInnerConatiner
+        }
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{children}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+export default PrimaryButton;
+
+const styles = StyleSheet.create({
+  buttonOuterConatiner: {
+    borderRadius: 28,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerConatiner: {
+    backgroundColor: "#72063c",
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    elevation: 2,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  },
+});
---
+++ Guess-Game/screens/GameScreen.js
@@ -0,0 +1,3 @@
+function GameScreeen() {}
+
+export default GameScreeen;
