2d51c6455c2ca309d59299e193c806ca50719d3c
Mukesh Venkat <mukiv0802@gmail.com>
2023-10-05 10:08:06 +0530

    Styling the main screen.

--- Guess-Game/App.js
+++ Guess-Game/App.js
@@ -1,10 +1,22 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, StyleSheet, View } from "react-native";
 import StartGameScreeen from "./screens/StartGameScreen";
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-  return <StartGameScreeen />;
+  return (
+    <LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootContainer}>
+      <ImageBackground source={require('./assets/Images/background.png')} style={styles.rootContainer} imageStyle={styles.backgroundImage}>
+        <StartGameScreeen />
+      </ImageBackground>
+    </LinearGradient>
+  );
 }
 
 const styles = StyleSheet.create({
+  rootContainer: {
+    flex: 1
+  },
+  backgroundImage: {
+    opacity: 0.15
+  }
 });
--- Guess-Game/screens/StartGameScreen.js
+++ Guess-Game/screens/StartGameScreen.js
@@ -12,8 +12,14 @@ function StartGameScreeen() {
         autoComplete="off"
         autoCapitalize="none"
       />
-      <PrimaryButton>Reset</PrimaryButton>
-      <PrimaryButton>Confirm</PrimaryButton>
+      <View style={styles.buttonViewContainer}>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Reset</PrimaryButton>
+        </View>
+        <View style={styles.buttonContainer}>
+          <PrimaryButton>Confirm</PrimaryButton>
+        </View>
+      </View>
     </View>
   );
 }
@@ -22,16 +28,24 @@ export default StartGameScreeen;
 
 const styles = StyleSheet.create({
   inputContainer: {
+    justifyContent: 'center',
+    alignItems: 'center',
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: "#4e0329",
+    backgroundColor: "#3b021f",
     borderRadius: 8,
     elevation: 4,
     shadowColor: "black",
     shadowOffset: { width: 0, height: 2 },
     shadowRadius: 6,
     shadowOpacity: 0.25,
+  },
+  buttonViewContainer: {
+    flexDirection: 'row'
+  },
+  buttonContainer: {
+    flex: 1
   },
   numberInput: {
     height: 50,
