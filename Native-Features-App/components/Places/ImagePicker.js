13e79650afdcc26648264cef1c922c0474d4eab5
Mukesh Venkat <mukiv0802@gmail.com>
2023-10-25 19:36:15 +0530

    Camera features with permission has been implemented.

--- Native-Features-App/components/Places/ImagePicker.js
+++ Native-Features-App/components/Places/ImagePicker.js
@@ -1,11 +1,14 @@
-import { Alert, Button, View } from "react-native";
+import { Alert, Image, StyleSheet, Text, View } from "react-native";
 import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+import { useState } from "react";
+import { Colors } from "../../constants/colors";
+import { OutlinedButton } from "../UI/OutlinedButton";
 
 export const ImagePicker = () => {
+    const [pickedImaged, setPickedImage] = useState('');
     const [cameraPermissionInformation, requestPermission] = useCameraPermissions();
 
     const verifyPermission = async () => {
-        await askPermissionsAsync();
         if (cameraPermissionInformation === PermissionStatus.UNDETERMINED) {
             const response = await requestPermission();
             return response.granted;
@@ -30,13 +33,40 @@ export const ImagePicker = () => {
             quality: 0.5
         });
         console.log(image);
+        setPickedImage(image.uri);
+    }
+
+    let imagePreview = pickedImaged;
+
+    if (imagePreview === '') {
+        imagePreview = <Text>No image found</Text>
+    } else {
+        imagePreview = <Image style={styles.image} source={{ uri: pickedImaged }} />
     }
 
     return (
         <View>
-            <View></View>
-            <Button title="Take Image" onPress={takeImageHandler} />
+            <View style={styles.imagePreview}>
+                {imagePreview}
+            </View>
+            <OutlinedButton icon='camera' onPress={takeImageHandler}>Take Image</OutlinedButton>
         </View>
     )
 };
+
+const styles = StyleSheet.create({
+    imagePreview: {
+        width: '100%',
+        height: 200,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginVertical: 8,
+        backgroundColor: Colors.primary100,
+        borderRadius: 4
+    },
+    image: {
+        width: '100%',
+        height: '100%'
+    }
+})
 
